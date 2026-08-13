b73664731e7e0f561db71ccee4fa86722bcc3afa
sivani <sivanimkumar7@gmail.com>
2026-08-13 13:04:26 +0530

    Refactor attendance script to streamline total attendance calculation and output

--- attendance.py
+++ attendance.py
@@ -3,11 +3,12 @@ attendance=[18,20,19,15,21]
 fulldays=0
 total_attendance=0
 for students in attendance:
+   total_attendance += students
    if students >=20:
       print("FULL")
       fulldays +=1
    else:
       print("NOT FULL")
-      total_attendance += students
-      print("full days:",fulldays)
-      print("total attendance:",total_attendance)
+
+print("full days:",fulldays)
+print("total attendance:",total_attendance)
