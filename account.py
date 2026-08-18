85b3b751ba0b1fbbc1cd75c793578de3ef6f001f
sivani <sivanimkumar7@gmail.com>
2026-08-18 20:07:43 +0530

    Add account management system with SavingsAccount and CurrentAccount classes

---
+++ account.py
@@ -0,0 +1,45 @@
+
+class Account:
+    def __init__(self, name, balance):
+        self._name = name          
+        self._balance = balance    
+
+    
+    def __add__(self, other):
+        return self._balance + other._balance
+
+
+
+class SavingsAccount(Account):
+    def calculate_interest(self):
+        return self._balance * 0.05
+
+
+
+class CurrentAccount(Account):
+    def calculate_interest(self):
+        return self._balance * 0.02
+
+
+
+savings = SavingsAccount("Ravi", 10000)
+current = CurrentAccount("Anjali", 15000)
+
+
+print("Savings Account")
+print("Name:", savings._name)
+print("Balance:", savings._balance)
+print("Interest:", savings.calculate_interest())
+
+print()
+
+print("Current Account")
+print("Name:", current._name)
+print("Balance:", current._balance)
+print("Interest:", current.calculate_interest())
+
+print()
+
+
+total_balance = savings + current
+print("Total Balance:", total_balance)
